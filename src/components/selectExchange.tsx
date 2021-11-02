a5dec6bc3102a18e04b4d727f51f6a69a3e9ff3e
GeloUno <geloke59@gmail.com>
2021-11-02 19:23:16 +0100

    get Currency Code Country to optional Exchange

--- src/components/selectExchange.tsx
+++ src/components/selectExchange.tsx
@@ -1,6 +1,13 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { useQuery } from 'react-query';
+import { getCurrencyCodeCountry } from '../controllers/getCurrecyCodeContry';
 import OptionExchange from './optionExchange';
+import { AxiosError, AxiosResponse } from 'axios';
+import { ICurrenciesData } from '../models/currencyCodeData';
+import { ErrorDataFetch } from '../models/ErrorDataFetch';
+import { ICurrencyCodeCounytry } from '../models/ICurrencyCodeCounytry';
+import { currencyCodeCountryObjectToArray } from '../controllers/CurrencyCodeCountryObjectToArray';
 
 interface ISelectExchangeProps {
   currency: string;
@@ -8,6 +15,35 @@ interface ISelectExchangeProps {
 
 function SelectExchange({ currency }: ISelectExchangeProps) {
   const [showSelect, setshowSelect] = useState<boolean>(false);
+
+  const [currencyCodeContryArray, setCurrencyCodeContryArray] = useState<
+    Array<ICurrencyCodeCounytry> | undefined
+  >();
+
+  const {
+    data,
+    isError,
+    isLoading,
+  }: {
+    data: AxiosResponse<ICurrenciesData> | undefined;
+    isError: boolean;
+    isLoading: boolean;
+    error: AxiosError<ErrorDataFetch> | null;
+  } = useQuery('CurrencyCodeCountry', getCurrencyCodeCountry, {
+    staleTime: 6 * 60 * 60 * 1000,
+  });
+
+  useMemo(() => {
+    if (data != undefined) {
+      const currencyCodeCoutryArray = currencyCodeCountryObjectToArray(
+        data.data
+      );
+
+      setCurrencyCodeContryArray(currencyCodeCoutryArray);
+    }
+  }, [data]);
+
+  console.log(`data`, data);
   return (
     <div>
       <div
@@ -20,7 +56,9 @@ function SelectExchange({ currency }: ISelectExchangeProps) {
           {showSelect && <FiChevronUp />}
         </div>
       </div>
-      {showSelect && <OptionExchange />}
+      {showSelect && currencyCodeContryArray != undefined && (
+        <OptionExchange currencyCodeContryArray={currencyCodeContryArray} />
+      )}
     </div>
   );
 }
