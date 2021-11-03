91953ca3d9628d21cde76641444eb99373472e30
GeloUno <geloke59@gmail.com>
2021-11-03 12:49:38 +0100

    add URL_CURRENCY_EXCHANGE

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,3 @@
 export const URL_CURRENCY_CODE_CONUTRY = `https://free.currconv.com/api/v7/currencies?apiKey=${process.env.REACT_APP_CURRENCY_KEY_API}`
+
+export const URL_CURRENCY_EXCHANGE = `https://free.currconv.com/api/v7/convert?apiKey=${process.env.REACT_APP_CURRENCY_KEY_API}`
