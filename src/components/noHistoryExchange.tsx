d287fbd1015cc5736c6db73b14456f5f65dcdb6c
GeloUno <geloke59@gmail.com>
2021-11-04 21:53:26 +0100

    add No history in local storage

---
+++ src/components/noHistoryExchange.tsx
@@ -0,0 +1,9 @@
+function NoHistoryExchange() {
+  return (
+    <div className="flex w-full h-full justify-center items-center">
+      <p className="text-sm font-light">Brak historii konwersji walut</p>
+    </div>
+  );
+}
+
+export default NoHistoryExchange;
