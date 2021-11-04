e184561cd94578a3392aa0d01365f6749befe066
GeloUno <geloke59@gmail.com>
2021-11-04 20:40:36 +0100

    change position "konwerter walut"

--- src/components/sectionExchange.tsx
+++ src/components/sectionExchange.tsx
@@ -38,7 +38,7 @@ function SectionExchange() {
 
   return (
     <div
-      className="flex absolute top-14 left-0  w-screen  justify-center"
+      className="flex absolute top-60 -left-32  w-screen  justify-center"
       style={{ height: '503px' }}
     >
       <HistoryExchange width={width} />
