e489044cd07a7740171c8bf7c994d00d3f48aa4e
GeloUno <geloke59@gmail.com>
2021-11-04 21:42:33 +0100

    use getExchange in addExchange

--- src/controllers/addExchangeToLocalSrorage.ts
+++ src/controllers/addExchangeToLocalSrorage.ts
@@ -1,4 +1,5 @@
 import { IExchangeLocalStorage } from "../models/IExchangeLocalStorage";
+import { getExchangeFromLocalStorage } from './getExchangeFromLocalSrorage';
 
 export function addExchangeToLocalStorage(
     valueFrom: string,
@@ -6,13 +7,13 @@ export function addExchangeToLocalStorage(
     valueTo: string,
     currencyTo: string,
 ) {
-    let dataLocalStore: Array<IExchangeLocalStorage> = [];
-    const dataFromLocalStorage = localStorage.getItem('exchange');
-    if (dataFromLocalStorage) {
-        dataLocalStore = JSON.parse(dataFromLocalStorage);
-    }
+    let dataLocalStore: Array<IExchangeLocalStorage> = getExchangeFromLocalStorage()
 
-    const exchangeDate = new Date();
+    const exchangeDate = new Date().toLocaleString('pl-PL', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
     const data: IExchangeLocalStorage = { valueFrom, currencyFrom, valueTo, currencyTo, exchangeDate };
 
     dataLocalStore.push(data);
